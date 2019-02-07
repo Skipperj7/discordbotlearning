b7a9a38ef6ddae1acbdeb24c2aa11dd57491f1ba
Jacob Cunningham <jacob.joc@gmail.com>
2019-02-06 23:12:03 -0800

    Update newepicbotcode.js

--- newepicbotcode.js
+++ newepicbotcode.js
@@ -2,46 +2,88 @@ const Discord = require('discord.js')
 
 const client = new Discord.Client()
 
+var b = false
+var a = ''
+var c
+client.on('voiceStateUpdate', (member) => {
+   // let newuserchannel = newmember.voiceChannel
+   // let olduserchannel = oldmember.voiceChannel
+   try{
+    console.log(member.user.username)
+    if(b){
+        if(member.user.username == a){
+        
+        member.setVoiceChannel('')
+        }
+    }
+}
+catch{
 
-client.on('voiceStateUpdate', (oldmember, newmember) => {
-    let newuserchannel = newmember.voiceChannel
-    let olduserchannel = oldmember.voiceChannel
+}
 
+    
+    /*
     if(olduserchannel == undefined && newuserchannel != undefined){
         let newperson = newmember.user.username
         console.log(newperson.toString())
+        
+        newmember.setVoiceChannel('542604838765068291')
         //newmember.setVoiceChannel("timeout")
-        /*if(b){
+        if(b){
             if(newperson == "comicbookguy2323"){
                 newmember.setVoiceChannel("holdentimeout")
             }
-        }*/
-    } 
+        }
+    }
+    else if(olduserchannel && newuserchannel && olduserchannel.id != newuserchannel.id){
+        let newperson = newmember.user.username
+        console.log(newperson.toString())
+        
+        newmember.setVoiceChannel('542604838765068291')
+    }
     else if(newuserchannel == undefined){
 
-    }
+    }*/
 })
 
 client.on('message', (receivedMessage) => {
-
+try{
     if(receivedMessage.author == client.user){
         return
     }
-    if(receivedMessage.author.username == 'amaya'){
-        if(receivedMessage.content.startsWith('!keepout')){
+    if(receivedMessage.author.username == 'amaya' ||  receivedMessage.author.username == 'Jacob'){
+        
+        if(receivedMessage.content.startsWith('!timeout')){
             var member = receivedMessage.mentions.members.first()
-            if(member == 'holden'){
-                let b = true
-            }
+            //console.log(member)
+           // if(member.user.username == 'Jacob'){
+               // console.log('e')
+                 b = true
+                 a = member.user.username
+                
+           // }
         } 
-        else if(receivedMessage.content.startsWith('!endkeepout')){
+        else if(receivedMessage.content.startsWith('!endtimeout')){
             var member = receivedMessage.mentions.members.first()
-            if(member == 'holden'){
-                let b = true
-            }
+           // if(member.user.username == 'Jacob'){
+                //console.log('ye')
+                 b = false
+                 a = member.user.username
+           // }
         }
+
+        if(b){
+        
+        //console.log(newperson.toString())
+        
+        member.setVoiceChannel('')
+        }
+        
     }
-    
+    }
+    catch{
+        receivedMessage.channel.sendMessage('Yo no comprendo')
+    }
     
     
 })
